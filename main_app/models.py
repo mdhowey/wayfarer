6f22f1aed86eb406b778398e7dd48b428b503cd6
Phil-Welsh <pwelsh8@gmail.com>
2021-10-31 16:14:45 -0700

    Added post to admin models

--- main_app/models.py
+++ main_app/models.py
@@ -10,9 +10,6 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
 
-    def __str__(self):
-        return self.name
-
 
 
 # Profile extension commented out (Howey and Michael approach below)
