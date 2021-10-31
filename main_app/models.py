b9230724f0d09294b1798b681cd37a20efd76804
Phil-Welsh <pwelsh8@gmail.com>
2021-10-31 16:07:24 -0700

    Added Post model

--- main_app/models.py
+++ main_app/models.py
@@ -1,7 +1,17 @@
 # from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
 
+class Post(models.Model):
 
+    title = models.CharField(max_length=100)
+    img = models.CharField(max_length=250)
+    body = models.TextField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+
+    def __str__(self):
+        return self.name
 
 
 
