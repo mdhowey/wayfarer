7cf1657c7efd653784459b405059241127b3f93d
Phil-Welsh <pwelsh8@gmail.com>
2021-10-31 18:46:50 -0700

    Made city model

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-from .models import Post
+from .models import City, Post
 
 # Profile extension commented out
 # from .models import Profile
 
 # Register your models here.
 admin.site.register(Post)
+admin.site.register(City)
 
 # Profile extension commented out
 # admin.site.register(Profile)
