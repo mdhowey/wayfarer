fb504a010246d01ddce4e407822c4e2b7a4a98cf
devhowey <dev.howey@gmail.com>
2021-11-03 17:13:57 -0600

    comment model added

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import City, Post, Profile
+from .models import City, Post, Profile, Comment
 
 # Profile extension commented out
 # from .models import Profile
@@ -8,6 +8,7 @@ from .models import City, Post, Profile
 admin.site.register(Post)
 admin.site.register(City)
 admin.site.register(Profile)
+admin.site.register(Comment)
 
 # Profile extension commented out
 # admin.site.register(Profile)
