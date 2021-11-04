c5e8d9f2dc40106235b8e5266af3ac56c46f5cab
Michael Karr <michaelhkarr@gmail.com>
2021-11-04 14:07:31 -0500

    retry no secret heroku config

--- wayfarer/settings.py
+++ wayfarer/settings.py
@@ -21,11 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
-SECRET_KEY = SECRET_KEY
+# SECRET_KEY = SECRET_KEY
 # Application definition
 
 INSTALLED_APPS = [
